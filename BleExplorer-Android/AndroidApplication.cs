d3f9b48818ecbddd564015c69f4b9aba190c28e9
Static Panda <p.m.banka@gmail.com>
2015-05-02 09:52:15 +0200

    Rename Project to BleExplorer

---
+++ BleExplorer-Android/AndroidApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using ReactiveUI;
+using BleExplorer.Core.ViewModels;
+
+namespace BleExplorer
+{
+    [Application(Label = "BleExplorer-Android")]
+    public class AndroidApplication : Application
+    {
+        AutoSuspendHelper autoSuspendHelper;
+        public AndroidApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle,transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+                        
+            autoSuspendHelper = new AutoSuspendHelper(this);
+            RxApp.SuspensionHost.CreateNewAppState = () => {
+                return new AppBootstrapper();
+            };
+
+            RxApp.SuspensionHost.SetupDefaultSuspendResume();
+        }
+    }
+}
+
