f906eb6b009993e20f32d71d7e8bb29cdfa0097a
Static Panda <p.m.banka@gmail.com>
2015-05-05 01:50:11 +0200

    Added Ensure utils class.

---
+++ BleExplorer-Core/Utils/Ensure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace BleExplorer.Core.Utils
+{
+    public static class Ensure
+    {
+        [NotNull]
+        public static T NotNull<T>([CanBeNull]T argument, string argumentName)
+        {
+            if (argument == null) throw new ArgumentNullException(argumentName);
+            return argument;
+        }
+    }
+}
