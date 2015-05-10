9498cf4986264d0f0de26398f78b00e02b130fac
Static Panda <p.m.banka@gmail.com>
2015-05-10 21:30:06 +0200

    Added handler for user error in Android app.

--- BleExplorer-Android/MainActivity.cs
+++ BleExplorer-Android/MainActivity.cs
@@ -1,5 +1,7 @@
+using System.Reactive.Linq;
 using Android.App;
 using Android.OS;
+using Android.Widget;
 using BleExplorer.Core.ViewModels;
 using ReactiveUI;
 using Splat;
@@ -16,8 +18,14 @@ namespace BleExplorer
             base.OnCreate(bundle);
             Forms.Init(this, bundle);
 
-            var bootstrapper = RxApp.SuspensionHost.GetAppState<AppBootstrapper>();
+            UserError.RegisterHandler(ue =>
+            {
+                var toast = Toast.MakeText(this, ue.ErrorMessage, ToastLength.Long);
+                toast.Show();
+                return Observable.Return(RecoveryOptionResult.CancelOperation);
+            });
 
+            var bootstrapper = RxApp.SuspensionHost.GetAppState<AppBootstrapper>();
 
             SetPage(bootstrapper.CreateMainPage());
         }
