e2251bc9001eddbd107cb242ef9b2a9b063f612b
Static Panda <p.m.banka@gmail.com>
2015-05-18 14:36:29 +0200

    Added Rx-related extensions.

---
+++ BleExplorer-Core/Utils/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace BleExplorer.Core.Utils
+{
+    public static class Extensions
+    {
+        public static IObservable<Unit> ToUnit<T>(this IObservable<T> self)
+        {
+            return self.Select(_ => Unit.Default);
+        }
+
+        public static IObservable<object> ToObject(this IObservable<Unit> self)
+        {
+            return self.Select(_ => (object) null);
+        }
+
+        public static ReactiveCommand<Unit> NavigateCommandFor<T>(this RoutingState router, T viewModel)
+            where T : IRoutableViewModel
+        {
+            return ReactiveCommand.CreateAsyncObservable(
+                router.Navigate.CanExecuteObservable,
+                _ => router.Navigate.ExecuteAsync(viewModel).ToUnit());
+        }
+    }
+}
